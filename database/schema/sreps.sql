a727e2db9baf85d4ae2657cb011d1f79e6196405
4941748 <4941748@student.swin.edu.au>
2016-10-04 12:29:19 +1100

    Updated DB schema script

--- database/schema/sreps.sql
+++ database/schema/sreps.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50710
 File Encoding         : 65001
 
-Date: 2016-09-04 20:00:38
+Date: 2016-10-04 12:28:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,7 @@ CREATE TABLE `customers` (
   `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` int(10) NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `medicare_num` int(11) DEFAULT NULL,
+  `medicare_num` bigint(11) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -44,11 +44,14 @@ CREATE TABLE `customers` (
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `qty` int(2) DEFAULT NULL,
-  `price` double(4,2) DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `qty` int(11) DEFAULT NULL,
+  `price` double(10,2) NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo_dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -63,14 +66,15 @@ DROP TABLE IF EXISTS `purchase_orders`;
 CREATE TABLE `purchase_orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `staff_id` int(11) NOT NULL,
-  `customers_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `total` double(10,2) NOT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `staff_id` (`staff_id`),
-  KEY `customers_id` (`customers_id`),
+  KEY `customers_id` (`customer_id`),
   CONSTRAINT `purchase_orders_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`),
-  CONSTRAINT `purchase_orders_ibfk_2` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`)
+  CONSTRAINT `purchase_orders_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
@@ -78,20 +82,24 @@ CREATE TABLE `purchase_orders` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for purchase_orders_products
+-- Table structure for purchase_order_line_item
 -- ----------------------------
-DROP TABLE IF EXISTS `purchase_orders_products`;
-CREATE TABLE `purchase_orders_products` (
-  `purchase_orders_id` int(11) NOT NULL,
-  `products_id` int(11) NOT NULL,
-  PRIMARY KEY (`purchase_orders_id`,`products_id`),
-  KEY `products_id` (`products_id`),
-  CONSTRAINT `purchase_orders_products_ibfk_1` FOREIGN KEY (`purchase_orders_id`) REFERENCES `purchase_orders` (`id`),
-  CONSTRAINT `purchase_orders_products_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `purchase_order_line_item`;
+CREATE TABLE `purchase_order_line_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `purchase_order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `price` double(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `purchase_order_id` (`purchase_order_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `purchase_order_line_item_ibfk_1` FOREIGN KEY (`purchase_order_id`) REFERENCES `purchase_orders` (`id`),
+  CONSTRAINT `purchase_order_line_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
--- Records of purchase_orders_products
+-- Records of purchase_order_line_item
 -- ----------------------------
 
 -- ----------------------------
@@ -106,16 +114,16 @@ CREATE TABLE `staff` (
   `phone` int(10) NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `store_location` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `username` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password_hash` varchar(72) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(72) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of staff
 -- ----------------------------
-INSERT INTO `staff` VALUES ('1', 'Hamish', 'Dean', 'Somewhere', '420608139', 'hamish.dean@live.com.au', 'CBD', 'haymo', '$2a$10$E.gz.sMI1LkjUvN1v72iCeHP0pGOBC/ainYgFXMuqE.zuBTNuYA8m', '2016-09-04 19:40:56', '2016-09-04 19:40:56');
+INSERT INTO `staff` VALUES ('1', 'Hamish', 'Dean', 'Somewhere', '420608139', 'hamish.dean@live.com.au', 'CBD', 'hamish', '$2a$10$E.gz.sMI1LkjUvN1v72iCeHP0pGOBC/ainYgFXMuqE.zuBTNuYA8m', '2016-09-04 19:40:56', '2016-09-04 19:40:56');
